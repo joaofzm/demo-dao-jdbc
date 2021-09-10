3d65bd3f1592ae115f37c36989266952ae401ccb
joaofzm <joao.fzm@hotmail.com>
2021-09-09 22:28:54 -0300

    Created Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entitites.Department;
+import model.entitites.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
+		Department obj = new Department(1, "Babybay");
+
+		Seller seller = new Seller(21, "Braia", "braia@gmail.com", new Date(), 3000.0, obj);
+
+		System.out.println(seller);
 		System.out.println(obj);
 	}
 }
